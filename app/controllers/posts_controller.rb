da203c8fc97092fb497dbdd54fd69d90864c46de
joe-tripodi <joe@flowofcode.com>
2024-12-24 09:50:08 +1030

    only list published posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all.order(published_date: "asc")
+    @posts = Post.where(status: "published").order(published_date: "desc")
   end
 
   # GET /posts/1 or /posts/1.json
